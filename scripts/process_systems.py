bdcbdd2405873ac5e97f6f592c12448105e24adf
Rafael Ferreira da Silva <silvarf@ornl.gov>
2022-01-10 15:59:35 -0500

    workflow systems

--- scripts/process_systems.py
+++ scripts/process_systems.py
@@ -12,6 +12,10 @@ import json
 from string import Template
 
 
+def _get_value(key, obj, default_value):
+    return obj[key] if key in obj else default_value
+
+
 systems = []
 headers = {
     "Accept": "application/vnd.github.mercy-preview+json",
@@ -22,112 +26,141 @@ headers = {
 with open("_data/workflow_systems.yml") as f:
     systems = yaml.safe_load(f)
 
-for w in systems:
-    print(w)
-    url = f"https://api.github.com/repos/{w['organization']}/{w['repository']}"
+for s in systems:
+    print(s)
+    url = f"https://api.github.com/repos/{s['organization']}/{s['repository']}"
     r = requests.get(url, headers=headers)
     response = r.json()
 
     # repo general information
-    w["title"] = response["name"]
-    w["default_branch"] = response["default_branch"]
-    w["subtitle"] = response["description"]
-    w["description"] = response["description"]
-    w["license"] = response["license"]["spdx_id"] if response["license"] else "No license available"
-    w["issues"] = response["open_issues"]
-    w["forks"] = response["forks"]
-    w["stargazers"] = response["stargazers_count"]
-    w["avatar"] = response["owner"]["avatar_url"]
-    w["website"] = response["homepage"]
-    w["twitter"] = ""
-    w["youtube"] = ""
-    w["doc_general"] = ""
-    w["doc_installation"] = ""
-    w["doc_tutorial"] = ""
+    s["title"] = response["name"]
+    s["default_branch"] = response["default_branch"]
+    s["subtitle"] = response["description"]
+    s["description"] = response["description"]
+    s["license"] = response["license"]["spdx_id"] if response["license"] else "No license available"
+    s["issues"] = response["open_issues"]
+    s["forks"] = response["forks"]
+    s["stargazers"] = response["stargazers_count"]
+    s["avatar"] = response["owner"]["avatar_url"]
+    s["website"] = response["homepage"]
+    s["twitter"] = ""
+    s["youtube"] = ""
+    s["language"] = ""
+    s["doc_general"] = ""
+    s["doc_installation"] = ""
+    s["doc_tutorial"] = ""
+    s["execution_environment"] = ""
 
     # date
     date = datetime.datetime.strptime(
         response["updated_at"], "%Y-%m-%dT%H:%M:%SZ")
-    w["year"] = date.strftime("%Y")
-    w["month"] = date.strftime("%b")
-    w["monthn"] = date.strftime("%m")
-    w["day"] = date.strftime("%d")
+    s["year"] = date.strftime("%Y")
+    s["month"] = date.strftime("%b")
+    s["monthn"] = date.strftime("%m")
+    s["day"] = date.strftime("%d")
 
     # topics
-    w["topics"] = "No topics available"
-    w["tags"] = ""
+    s["topics"] = "No topics available"
+    s["tags"] = ""
     topics = response["topics"]
     if len(topics) > 0:
-        w["topics"] = ""
+        s["topics"] = ""
         for topic in topics:
-            w["topics"] += f"<span class='topic'>{topic}</span>&nbsp;&nbsp;"
-            w["tags"] += f"{topic} "
+            s["topics"] += f"<span class='topic'>{topic}</span>&nbsp;&nbsp;"
+            s["tags"] += f"{topic} "
 
     # releases
     url = response['releases_url']
     r = requests.get(url[:-5], headers=headers)
     data = r.json()
-    w["release"] = ""
-    w["releases"] = len(data)
+    s["release"] = ""
+    s["releases"] = len(data)
     if len(data) > 0:
-        date = datetime.datetime.strptime(data[0]["published_at"], "%Y-%m-%dT%H:%M:%SZ")
+        date = datetime.datetime.strptime(
+            data[0]["published_at"], "%Y-%m-%dT%H:%M:%SZ")
         release_date = date.strftime("%d %b %Y")
-        w["release"] = f"<a href=\"{data[0]['html_url']}\" target=\"_blank\" class=\"release-button\">{data[0]['name']} " \
+        s["release"] = f"<a href=\"{data[0]['html_url']}\" target=\"_blank\" class=\"release-button\">{data[0]['name']} " \
             f"<span class=\"light-gray\">({release_date})</span></a>"
 
     # contributors
     r = requests.get(response['contributors_url'], headers=headers)
     data = r.json()
-    w['contributors'] = len(data)
-    w['contributors_list'] = ""
+    s["contributors"] = len(data)
+    s["contributors_list"] = ""
     for c in data:
-        w["contributors_list"] += f'<a href="{c["html_url"]}" target="_blank"><img src="{c["avatar_url"]}" width="32" height="32" alt="{c["login"]}"/></a>'
+        s["contributors_list"] += f'<a href="{c["html_url"]}" target="_blank"><img src="{c["avatar_url"]}" width="32" height="32" alt="{c["login"]}"/></a>'
 
     # metadata file
     r = requests.get(
-        f"https://raw.githubusercontent.com/{w['organization']}/{w['repository']}/{w['default_branch']}/.wci.yml")
+        f"https://raw.githubusercontent.com/{s['organization']}/{s['repository']}/{s['default_branch']}/.wci.yml")
 
     if r.ok:
         data = yaml.safe_load(r.text)
         print(data)
 
-        if "name" in data:
-            w["title"] = data["name"]
-
         # description
-        if "headline" in data:
-            w["subtitle"] = data["headline"]
-        if "description" in data:
-            w["description"] = data["description"]
-        if "website" in data:
-            w["website"] = data["website"]
+        s["title"] = _get_value("name", data, s["title"])
+        s["subtitle"] = _get_value("headline", data, s["subtitle"])
+        s["description"] = _get_value("description", data, s["description"])
+        s["website"] = _get_value("website", data, s["website"])
+        
+        # language
+        if "language" in data:
+            s["language"] = f"<li><i class=\"fas fa-code\"></i><br />{data['language']}</li>"
 
         # social
         if "social" in data:
             if "twitter" in data["social"]:
-                w["twitter"] = "<li>" \
+                s["twitter"] = "<li>" \
                     f"<a href=\"https://twitter.com/{data['social']['twitter']}\" target=\"_blank\" class=\"fa-stack fa-2x\">" \
                     "<i class=\"fa fa-circle fa-stack-2x\" style=\"color: #fff\"></i><i class=\"fab fa-twitter fa-stack-1x\"></i></a></li>"
 
             if "youtube" in data["social"]:
-                w["youtube"] = "<li>" \
+                s["youtube"] = "<li>" \
                     f"<a href=\"{data['social']['youtube']}\" target=\"_blank\" class=\"fa-stack fa-2x\">" \
                     "<i class=\"fa fa-circle fa-stack-2x\" style=\"color: #fff\"></i><i class=\"fab fa-youtube fa-stack-1x\"></i></a></li>"
 
         # documentation
         if "documentation" in data:
-            w["doc_general"] = f"<li><a href=\"{data['documentation']['general']}\" target=\"_blank\"><i class=\"fas fa-book\"></i>Documentation</a></li>" if "general" in data["documentation"] else ""
-            w["doc_installation"] = f"<li><a href=\"{data['documentation']['installation']}\" target=\"_blank\"><i class=\"fas fa-download\"></i>Installation</a></li>" if "installation" in data["documentation"] else ""
-            w["doc_tutorial"] = f"<li><a href=\"{data['documentation']['tutorial']}\" target=\"_blank\"><i class=\"fas fa-user-cog\"></i>Tutorial</a></li>" if "tutorial" in data["documentation"] else ""
+            s["doc_general"] = f"<li><a href=\"{data['documentation']['general']}\" target=\"_blank\"><i class=\"fas fa-book\"></i><br />Documentation</a></li>" if "general" in data["documentation"] else ""
+            s["doc_installation"] = f"<li><a href=\"{data['documentation']['installation']}\" target=\"_blank\"><i class=\"fas fa-download\"></i><br />Installation</a></li>" if "installation" in data["documentation"] else ""
+            s["doc_tutorial"] = f"<li><a href=\"{data['documentation']['tutorial']}\" target=\"_blank\"><i class=\"fas fa-user-cog\"></i><br />Tutorial</a></li>" if "tutorial" in data["documentation"] else ""
+
+        # execution environment
+        if "execution_environment" in data:
+            s["execution_environment"] = "<div class=\"system-info\"><h1>Execution Environment</h1>" 
+
+            # user interfaces
+            if "interfaces" in data["execution_environment"]:
+                s["execution_environment"] += "<div class=\"system-info-sub\"><h2>User Interfaces</h2><ul class=\"list-tags color-2\">"
+                for i in data["execution_environment"]["interfaces"]:
+                    s["execution_environment"] += f"<li>{i}</li>"
+                s["execution_environment"] += "</ul></div>"
+
+            # resource managers
+            if "resource_managers" in data["execution_environment"]:
+                s["execution_environment"] += "<div class=\"system-info-sub\"><h2>Supported Resource Managers</h2><ul class=\"list-tags color-4\">"
+                for rm in data["execution_environment"]["resource_managers"]:
+                    s["execution_environment"] += f"<li>{rm}</li>"
+                s["execution_environment"] += "</ul></div>"
+
+            # transfer protocols
+            if "transfer_protocols" in data["execution_environment"]:
+                s["execution_environment"] += "<div class=\"system-info-sub\"><h2>Supported Transfer Protocols</h2><ul class=\"list-tags color-6\">"
+                for tp in data["execution_environment"]["transfer_protocols"]:
+                    s["execution_environment"] += f"<li>{tp}</li>"
+                s["execution_environment"] += "</ul></div>"
+
+        s["execution_environment"] += "</div>"
 
     # fill template
     with open('scripts/systems.html.in') as f:
         template = Template(f.read())
-        contents = template.substitute(w)
+        contents = template.substitute(s)
 
         from pathlib import Path
         Path("_systems").mkdir(parents=True, exist_ok=True)
 
         # write workflows data
-        with open(f"_systems/{w['repository']}.html", 'w') as f:
+        with open(f"_systems/{s['repository']}.html", 'w') as f:
             f.write(contents)
