3cf828c9fc9e521291568301e1b894d3eb77a857
Yaroslav Halchenko <debian@onerussian.com>
2023-09-29 15:54:17 -0400

    [DATALAD RUNCMD] run codespell throughout fixing typo automagically
    === Do not change lines below ===
    {
     "chain": [],
     "cmd": "codespell -w",
     "exit": 0,
     "extra_inputs": [],
     "inputs": [],
     "outputs": [],
     "pwd": "."
    }
    ^^^ Do not change lines above ^^^

--- assets/js/main.js
+++ assets/js/main.js
@@ -280,7 +280,7 @@
     },
     zoom: {
       enabled: true,
-      duration: 300 // don't foget to change the duration also in CSS
+      duration: 300 // don't forget to change the duration also in CSS
     }
   });
 
